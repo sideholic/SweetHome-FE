008e478da43dd38cb01ddf37a191a5abc4390acc
jundoh <jundo1302@gmail.com>
2022-03-08 00:46:16 +0900

    feat: footer markup done

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,81 @@
-import { FooterWrapper } from "../../styles/FooterStyle";
+import {
+  FooterFlexContent,
+  FooterText,
+  FooterWrapper,
+  TextWrapper,
+} from "../../styles/FooterStyle";
 import { CommInner } from "../../styles/GlobalStyle";
-import { CommonTitle, CommonTitleDiv } from "../../styles/CommonStyle";
+import { Logo } from "../../styles/HeaderStyle";
+import React from "react";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Footer() {
   return (
     <FooterWrapper>
-      <CommonTitleDiv>
-        <CommInner>
-          <CommonTitle>푸터</CommonTitle>
-        </CommInner>
-      </CommonTitleDiv>
+      <CommInner>
+        <FooterFlexContent>
+          <Logo>SWEET HOME</Logo>
+          <TextWrapper>
+            <FooterText>
+              <span className="leftSpan">TEAM</span>
+              <span>
+                SIDEHOLIC &copy;&nbsp;
+                <a
+                  href="https://github.com/sideholic"
+                  target="_blank"
+                  rel="noreferrer nofollow"
+                >
+                  <AiFillGithub />
+                </a>
+              </span>
+            </FooterText>
+            <FooterText>
+              <span>FE</span>
+              <span>
+                전도현&nbsp;
+                <a
+                  href="https://github.com/jeondoh"
+                  target="_blank"
+                  rel="noreferrer nofollow"
+                >
+                  <AiFillGithub />
+                </a>
+              </span>
+            </FooterText>
+            <FooterText>
+              <span className="leftSpan">EMAIL</span>
+              <span>jundo1302@gmail.com</span>
+            </FooterText>
+            <FooterText>
+              <span>BE</span>
+              <span>
+                박동환&nbsp;
+                <a
+                  href="https://github.com/dong-park"
+                  target="_blank"
+                  rel="noreferrer nofollow"
+                >
+                  <AiFillGithub />
+                </a>
+              </span>
+            </FooterText>
+            <FooterText />
+            <FooterText>
+              <span>SUPPORT</span>
+              <span>
+                이연주&nbsp;&nbsp;이은아&nbsp;
+                <a
+                  href="https://github.com/eunalee"
+                  target="_blank"
+                  rel="noreferrer nofollow"
+                >
+                  <AiFillGithub />
+                </a>
+              </span>
+            </FooterText>
+          </TextWrapper>
+        </FooterFlexContent>
+      </CommInner>
     </FooterWrapper>
   );
 }
--- src/styles/FooterStyle.ts
+++ src/styles/FooterStyle.ts
@@ -2,7 +2,51 @@ import styled from "styled-components";
 
 /* Wrapper */
 export const FooterWrapper = styled.footer`
-  background-color: ${(props) => props.theme.greenColor.green3};
-  height: 17.813rem;
+  background-color: #f2f2f2;
+  height: 16.25rem;
   margin-top: 6.25rem;
+`;
+/* 푸터 flex 지정 */
+export const FooterFlexContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  height: 100%;
+  padding: 2.5rem 0;
+`;
+
+export const TextWrapper = styled.div`
+  width: 100%;
+  border-top: 1px solid lightgray;
+  border-bottom: 1px solid lightgray;
+  padding: 1.25rem 0;
+  margin-top: 1.25rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+`;
+
+/* 푸터 글 */
+export const FooterText = styled.p`
+  width: 100%;
+  line-height: 1.8;
+  font-size: 0.875rem;
+
+  span {
+    display: inline-block;
+    &:nth-of-type(1) {
+      width: 3.571em;
+      margin-right: 2.143em;
+    }
+    &:nth-of-type(2) {
+      width: 14.286em;
+      &:before {
+        content: "|";
+        margin-right: 0.714em;
+      }
+    }
+    &.leftSpan {
+      margin-right: 1.071em;
+    }
+  }
 `;
